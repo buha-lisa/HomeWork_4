6575e3a88a5f67836bac95fa05fd0778d65f8fff
buha-lisa <bugaliza77@gmail.com>
2024-03-04 12:01:13 +0200

    Add project files.

---
+++ hw4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace hw4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ hw4/homeWork.cs
@@ -0,0 +1,42 @@
+﻿namespace hw4
+{
+    internal class homeWork
+    {
+        public static void Task1()
+        {
+            Console.WriteLine("Choose the option:\n1.From binary to decimal\n2.From decimal to binary");
+            Console.Write("Your choice: ");
+            int.TryParse(Console.ReadLine(), out int choice);
+            if (choice == 1)
+            {
+                string str = Console.ReadLine();
+                int res = 0;
+                try
+                {
+                    if (str.Length > int.MaxValue)
+                    {
+                        throw new Exception("Error..");
+                    }
+                    int exponent = 1;
+                    for (int i = str.Length - 1; i >= 0; i--)
+                    {
+                        if (str[i] == '1')
+                        {
+                            res += exponent;
+                        }
+                        exponent *= 2;
+                    }
+                    Console.WriteLine(res);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("End...");
+                }
+            }
+        }
+    }
+}
